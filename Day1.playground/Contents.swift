7d2fb5f5cb5d3710991e988cb40e580299560aa2
emir <emirkartal00@gmal.com>
2022-03-03 12:10:26 +0300

    Add Day1

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,43 @@
+import UIKit
+// Day1 Variables
+
+var str = "Welcome, 100DaysOfSwift"
+str = "Goodbye"
+
+var age = 38
+var population = 8_000_000
+
+// multi-line String
+var sentence = """
+multi-line
+string
+with swift
+"""
+var sentence2 = """
+multiline \
+string \
+test
+"""
+
+print(sentence)
+print(sentence2)
+
+var dbl = 3.141
+var bol = true
+
+// interpolation
+
+var score = 80
+var snt = "your score was \(score)"
+var snt2 = "The test result are here :\(snt)"
+
+// constants
+
+let myName = "emir"
+let myAge  = 20
+
+// type annotation
+
+let myPoint : Int = 97
+var myCourse : String = "Data Structures"
+var isTrue : Bool = true 
